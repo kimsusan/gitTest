029832da27008352f931682cc74b5393554689db
kosmo-03 <kosmo-03@kosmo-03-PC>
2018-07-10 10:57:37 +0900

    hello world

--- JAVA_Git/src/HelloGit.java
+++ JAVA_Git/src/HelloGit.java
@@ -2,5 +2,7 @@
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
 	}
 }
